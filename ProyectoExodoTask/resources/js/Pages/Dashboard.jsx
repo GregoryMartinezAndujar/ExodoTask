3b0f32533943ac76378d03a54bae6f083b868699
GregoryMA23 <gregorymartinez.gym@gmail.com>
2026-03-22 13:13:14 +0100

    filtrado de tareas por pendientes y completadas

--- ProyectoExodoTask/resources/js/Pages/Dashboard.jsx
+++ ProyectoExodoTask/resources/js/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import Tareas from "@/components/Tareas";
 import ApplicationLogo from "@/components/ApplicationLogo";
+import { useState } from "react";
 export default function Dashboard({
     tareas,
     currentRoute,
@@ -15,6 +16,13 @@ export default function Dashboard({
     let pendientes = tareas.filter(
         (tarea) => tarea.a_completada === false,
     ).length;
+
+    const [verSegunEstado, setVerSegunEstado] = useState(false);
+
+    const tareasFiltradas = verSegunEstado
+        ? tareas.filter((tarea) => tarea.a_completada === true)
+        : tareas.filter((tarea) => tarea.a_completada === false);
+
     return (
         <AuthenticatedLayout
             auth={auth}
@@ -30,29 +38,29 @@ export default function Dashboard({
         >
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
                 {/* Completadas */}
-                <div className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-200 hover:shadow-md transition">
+                <div
+                    className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-200 hover:shadow-md transition"
+                    onClick={() => setVerSegunEstado(true)}
+                >
                     <div className="w-12 h-12 flex items-center justify-center rounded-full bg-green-100 text-green-600 text-xl">
                         ✓
                     </div>
                     <div>
-                        <p className="text-2xl  text-gray-900">
-                            {completadas}
-                        </p>
+                        <p className="text-2xl  text-gray-900">{completadas}</p>
                         <p className="text-gray-700 font-medium">Completadas</p>
-
                     </div>
                 </div>
                 {/* Pendientes */}
-                <div className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-200 hover:shadow-md transition">
-                    <div className="w-12 h-12 flex items-center justify-center rounded-full bg-red-100 text-red-600 text-xl">
+                <div
+                    className="flex items-center gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-200 hover:shadow-md transition"
+                    onClick={() => setVerSegunEstado(false)}
+                >
+                    <div className="w-12 h-12 flex items-center justify-center rounded-full bg-yellow-100 text-yellow-600 text-xl">
                         !
                     </div>
                     <div>
-                        <p className="text-2xl  text-gray-900">
-                            {pendientes}
-                        </p>
+                        <p className="text-2xl  text-gray-900">{pendientes}</p>
                         <p className="text-gray-700 font-medium">Pendientes</p>
-
                     </div>
                 </div>
             </div>
@@ -60,7 +68,7 @@ export default function Dashboard({
             <Head title="Página Principal" />
 
             <div className="space-y-4">
-                {tareas.map((tarea) => (
+                {tareasFiltradas.map((tarea) => (
                     <Tareas
                         key={tarea.id}
                         tarea={tarea}
