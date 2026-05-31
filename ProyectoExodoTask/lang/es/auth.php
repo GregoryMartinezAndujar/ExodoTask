a3f660797095a8d7d5b9cb68b0bd236c3ed9462f
Gregory <gregorymartinez.gym@gmail.com>
2026-05-31 10:24:57 +0000

    feat: mensajes de error en español (login y registro)

---
+++ ProyectoExodoTask/lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Credenciales incorrectas.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos. Intenta de nuevo en :seconds segundos.',
+
+];
